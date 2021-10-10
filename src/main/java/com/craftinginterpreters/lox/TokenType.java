3d0af97e594423e4cfa6364b73d0e83038126f1f
Sergey <dream.doomwalker@gmail.com>
2021-10-11 02:17:38 +0300

    init

---
+++ src/main/java/com/craftinginterpreters/lox/TokenType.java
@@ -0,0 +1,53 @@
+package com.craftinginterpreters.lox;
+
+public enum TokenType {
+
+    //single-char token
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+
+    // one or two char tokens
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    LESS,
+    LESS_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+
+    //Literals
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    //keywords
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF
+}
